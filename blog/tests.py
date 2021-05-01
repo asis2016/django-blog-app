272cf75df68aa21920a4af3949e2c7093c4f461b
Ashish <mhrj.asis@gmail.com>
2021-05-01 11:07:06 +0200

    Test cases.

--- blog/tests.py
+++ blog/tests.py
@@ -31,13 +31,22 @@ class BlogTest(TestCase):
     def test_post_list_view(self):
         response = self.client.get(reverse('home'))
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'Nice body content')
+        self.assertContains(response, 'A good title')
         self.assertTemplateUsed(response, 'home.html')
 
-    def test_post_detail_view(self):
-        response = self.client.get('/post/1/')
+    def test_post_detail_view_no_response(self):
         no_response = self.client.get('/post/1000/')
-        self.assertEqual(response.status_code, 404)
         self.assertEqual(no_response.status_code, 404)
-        self.assertContains(response, 'A good title')
-        self.assertTemplateUsed(response, 'detail.html')
+
+    def test_post_detail_view_response(self):
+        response = self.client.get('/post/1/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_detail_view_response_template_and_contains(self):
+        """
+        To test:    1. Assert a response
+                    2. Asserts a template rendering the response
+        """
+        r = self.client.get('/post/1/')
+        self.assertContains(r, 'A good title')
+        self.assertTemplateUsed(r, 'detail.html')
--- blog/views.py
+++ blog/views.py
@@ -6,12 +6,12 @@ from .models import Post
 class BlogListView(ListView):
     """ A blog list view for displaying all posts. """
     model = Post
-    template_name = 'blog/home.html'
+    template_name = 'home.html'
     context_object_name = 'posts'
 
 
 class BlogDetailView(DetailView):
     """ A blog detail view for displaying single post. """
     model = Post
-    template_name = 'blog/detail.html'
+    template_name = 'detail.html'
     context_object_name = 'post'
