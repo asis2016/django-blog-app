9ec63bd6b3769946896bf747b11081e4ee773403
Ashish <mhrj.asis@gmail.com>
2021-06-04 15:49:33 +0200

    add whitenoise

--- README.md
+++ README.md
@@ -1,3 +1,72 @@
+# A Basic Django Blog App
+This is a simple demonstration of Blog (app) made in Django.
+
+## Tech Stack
+Python, Django, Gunicorn, Heroku
+
+## Environment Variables
+To run this project, you will need to add the following environment variables
+
+`SECRET_KEY` `django-insecure-jhrq9t==0!ysg^6*ut@ort)!@wohm)g^k(xn@515$$=h39(6b(e`
+
+`ANOTHER_API_KEY`
+
+  
+## Run Locally
+
+Clone the project
+
+```bash
+  git clone https://link-to-project
+```
+
+Go to the project directory
+
+```bash
+  cd my-project
+```
+
+Install dependencies
+
+```bash
+  npm install
+```
+
+Start the server
+
+```bash
+  npm run start
+```
+
+  
+## Demo with Screenshots
+
+![App Screenshot](https://via.placeholder.com/468x300?text=App+Screenshot+Here)
+
+  
+## Roadmap
+
+- Additional browser support
+
+- Add more integrations
+
+  
+## Feedback
+
+If you have any feedback, please reach out to us at fake@fake.com
+
+  
+
+
+
+
+
+
+
+
+
+
+
 # A Simple Django Blog App
 
 ## v1
--- blog_project/settings.py
+++ blog_project/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'accounts',
     'blog',
@@ -41,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -114,7 +116,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
